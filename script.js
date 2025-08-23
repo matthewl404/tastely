6feee1e8d986de94a4febfc0eb9ba576b4e68064
matthewl404 <mli166100@gmail.com>
2025-08-23 02:47:08 -0400

    added js to test frontend

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+//TEST 
+document.getElementById("predictBtn").addEventListener("click", () => {
+  const ingredients = document.getElementById("ingredients").value.trim();
+  const resultDiv = document.getElementById("result");
+
+  if (!ingredients) {
+    resultDiv.innerText = "Please enter some ingredients first!";
+    return;
+  }
+
+  const predictions = [
+    "You’ll probably enjoy this dish!",
+    "This one might not suit your taste.",
+    "Could be a hit with your flavor preferences!",
+    "Not sure about this one, but give it a try!"
+  ];
+  
+  const randomIndex = Math.floor(Math.random() * predictions.length);
+  resultDiv.innerText = predictions[randomIndex];
+});
+
